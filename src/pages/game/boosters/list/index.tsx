577d6aea8d98ec17886dcb53ffc079fa32e29580
YuriiLohvynenko <polarbearbusiness@hotmail.com>
2024-08-10 11:52:30 +0200

    fix: card size

--- src/pages/game/boosters/list/index.tsx
+++ src/pages/game/boosters/list/index.tsx
@@ -3,7 +3,9 @@ import classNames from "../../../../consts/classNames";
 import all_boosters from "../../../../data/all_boosters.json";
 const AllBoosters = () => {
   return (
-    <div className={`${classNames.containerClass} mt-10 lg:mt-20`}>
+    <div
+      className={`${classNames.containerClass} lg:max-w-[1120px] mt-10 lg:mt-20`}
+    >
       {/* Cover */}
       <div className="text-center space-y-4">
         <h1 className="text-indigo-500 text-3xl font-bold lg:text-5xl">
@@ -13,7 +15,7 @@ const AllBoosters = () => {
           Our team includes the top boosters in the industry
         </p>
       </div>
-      <div className="mt-10 lg:mt-20 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-x-4 gap-y-6">
+      <div className="mt-10 lg:mt-20 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-x-4 gap-y-6 max-sm:max-w-[370px] mx-auto">
         {all_boosters?.map((d: any, index: number) => (
           <CardBooster {...d} key={index} />
         ))}
